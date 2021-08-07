839e468d383e8d21b30ecc16eb61c33656df3c71
rafaelprad <rafaelprad@gmail.com>
2021-08-06 23:55:56 -0300

    impl: adding a react child 1

--- src/App.test.tsx
+++ src/App.test.tsx
@@ -1,9 +1,9 @@
 import React from 'react';
 import { render, screen } from '@testing-library/react';
-import App from './App';
+import AppChild1 from './App';
 
 test('renders learn react link', () => {
-  render(<App />);
+  render(<AppChild1 />);
   const linkElement = screen.getByText(/learn react/i);
   expect(linkElement).toBeInTheDocument();
 });
